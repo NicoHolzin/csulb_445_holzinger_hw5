d446d2ce93e23baead4c9dc74b6133b5d00d9572
Nicolas Holzinger <nicoholzin@gmail.com>
2024-10-10 16:49:48 -0700

    v3

--- js/Exercice3.js
+++ js/Exercice3.js
@@ -8,3 +8,60 @@
 // Note:
 // Don’t use the Math.min() and/or the Math.max functions.
 // The objective is to familiarize you with the reduce method.
+
+//Declare studentList Array
+const studentList = [
+  {
+  firstName: "Allan",
+  lastName: "Able",
+  scores: [95, 85, 92, 98]
+  },
+  {
+  firstName: "Amy",
+  lastName: "Alexander",
+  scores: [80, 88, 100]
+  },
+  {
+  firstName: "Betty",
+  lastName: "Barns",
+  scores: [70, 80, 90, 100]
+  },
+  {
+  firstName: "Bob",
+  lastName: "Bones",
+  scores: [75, 85, 95, 85]
+  },
+  {
+  firstName: "Cindy",
+  lastName: "Chase",
+  scores: [95, 90, 92, 98]
+  },
+  {
+  firstName: "Charles",
+  lastName: "Chips",
+  scores: [88, 99, 90]
+  },
+  ];
+
+
+// TO DO - Write higher order functions / There are many solutions
+//Declare cLastNameResults. Use functional programming (filter, map, reduce, and pure functions) to generate a new array of objects (don’t use a traditional loop)
+
+const cLastNameResults = studentList
+  .filter(student => student.lastName.startsWith('C'))  
+  .map(student => {                                     
+    const totalScores = student.scores.reduce((sum, score) => sum + score, 0);
+    const minScore = student.scores.reduce((min, score) => (score < min ? score : min), student.scores[0]);
+    const maxScore = student.scores.reduce((max, score) => (score > max ? score : max), student.scores[0]);
+    const avgScore = totalScores / student.scores.length;
+    
+    return {
+      firstName: student.firstName,
+      lastName: student.lastName,
+      minScore: minScore,
+      maxScore: maxScore,
+      avgScore: avgScore.toFixed(2)
+    };
+  });
+
+console.log(cLastNameResults);
